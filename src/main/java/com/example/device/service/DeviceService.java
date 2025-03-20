8ed32fdceac674d2e7cb01ed4dd0f86fd422660d
Ricardo Ribeiro <you@example.com>
2025-03-20 13:31:09 -0300

    Service added

---
+++ src/main/java/com/example/device/service/DeviceService.java
@@ -0,0 +1,23 @@
+package com.example.device.service;
+
+public class DeviceService {
+
+    public Object create(Object deviceDTO){
+        return null;
+    }
+
+    public Object retrieve(Object deviceId){
+        return null;
+    }
+
+    public Object list(){
+        return null;
+    }
+
+    public Object update(Object deviceDTO){
+        return null;
+    }
+
+    public void delete(Object deviceId){
+    }
+}
