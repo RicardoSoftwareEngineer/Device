9392eff58f3acb8ec3aab3f6a983f67a90f9e092
Ricardo Ribeiro <you@example.com>
2025-03-20 21:03:12 -0300

    Service implementations added

--- src/main/java/com/example/device/service/DeviceService.java
+++ src/main/java/com/example/device/service/DeviceService.java
@@ -1,11 +1,16 @@
 package com.example.device.service;
 
 import com.example.device.dto.DeviceDTO;
+import com.example.device.entity.DeviceEntity;
+import com.example.device.entity.StateEnum;
 import com.example.device.repository.DeviceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DeviceService {
@@ -13,21 +18,66 @@ public class DeviceService {
     private DeviceRepository deviceRepository;
 
     public DeviceDTO create(DeviceDTO deviceDTO){
-        return null;
+        DeviceEntity deviceEntity = deviceRepository.save(new DeviceEntity(deviceDTO));
+        return new DeviceDTO(deviceEntity);
     }
 
-    public DeviceDTO retrieve(String deviceId){
-        return null;
+    public DeviceDTO retrieveById(String deviceId){
+        DeviceEntity deviceEntity = deviceRepository.findById(deviceId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Device not found"));
+        return new DeviceDTO(deviceEntity);
     }
 
-    public List<DeviceDTO> list(){
-        return null;
+    public List<DeviceDTO> retrieveAll() {
+        return deviceRepository.findAll()
+                .stream()
+                .map(DeviceDTO::new)
+                .collect(Collectors.toList());
     }
 
-    public DeviceDTO update(DeviceDTO deviceDTO){
-        return null;
+    public List<DeviceDTO> retrieveByBrand(String brand) {
+        return deviceRepository.findByBrand(brand)
+                .stream()
+                .map(DeviceDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<DeviceDTO> retrieveByState(StateEnum state) {
+        return deviceRepository.findByState(state)
+                .stream()
+                .map(DeviceDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public DeviceDTO update(String deviceId, DeviceDTO deviceDTO){
+        DeviceEntity deviceEntity = deviceRepository.findById(deviceId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Device not found"));
+        if(deviceEntity.getState().equals(StateEnum.IN_USE)){
+            if(deviceDTO.getName() != null && !deviceEntity.getName().equals(deviceDTO.getName())){
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Name and brand properties cannot be updated if the device is in use");
+            }
+            if(deviceDTO.getBrand() != null && !deviceEntity.getBrand().equals(deviceDTO.getBrand())){
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Name and brand properties cannot be updated if the device is in use");
+            }
+        }
+        if(deviceDTO.getName() != null){
+            deviceEntity.setName(deviceDTO.getName());
+        }
+        if(deviceDTO.getBrand() != null){
+            deviceEntity.setBrand(deviceDTO.getBrand());
+        }
+        if(deviceDTO.getState() != null){
+            deviceEntity.setState(deviceDTO.getState());
+        }
+        return new DeviceDTO(deviceRepository.save(deviceEntity));
     }
 
     public void delete(String deviceId){
+        DeviceEntity deviceEntity = deviceRepository.findById(deviceId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Device not found"));
+        if(deviceEntity.getState().equals(StateEnum.IN_USE)){
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "In use devices cannot be deleted");
+        }
+        deviceRepository.deleteById(deviceId);
     }
 }
