2417ad15913fb2ec96f103bd41291c2dbbe321f9
Ricardo Ribeiro <you@example.com>
2025-03-20 21:02:33 -0300

    Repository filters added

--- src/main/java/com/example/device/repository/DeviceRepository.java
+++ src/main/java/com/example/device/repository/DeviceRepository.java
@@ -1,10 +1,14 @@
 package com.example.device.repository;
 
 import com.example.device.entity.DeviceEntity;
+import com.example.device.entity.StateEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DeviceRepository extends JpaRepository <DeviceEntity, String> {
-    
+    List<DeviceEntity> findByBrand(String brand);
+    List<DeviceEntity> findByState(StateEnum state);
 }
