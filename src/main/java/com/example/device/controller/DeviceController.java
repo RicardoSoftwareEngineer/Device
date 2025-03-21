9661ea5364fe2694a95986aea0561270144bd157
Ricardo Ribeiro <you@example.com>
2025-03-20 21:01:41 -0300

    ResponseStatus added

--- src/main/java/com/example/device/controller/DeviceController.java
+++ src/main/java/com/example/device/controller/DeviceController.java
@@ -1,12 +1,13 @@
 package com.example.device.controller;
 
 import com.example.device.dto.DeviceDTO;
+import com.example.device.entity.StateEnum;
 import com.example.device.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class DeviceController {
@@ -14,26 +15,43 @@ public class DeviceController {
     private DeviceService deviceService;
 
     @PostMapping(value = "/device/v1")
+    @ResponseStatus(HttpStatus.CREATED)
     public DeviceDTO create(@RequestBody DeviceDTO deviceDTO){
         return deviceService.create(deviceDTO);
     }
 
     @GetMapping(value = "/device/v1/{id}")
-    public DeviceDTO retrieve(@PathVariable String id){
-        return deviceService.retrieve(id);
+    @ResponseStatus(HttpStatus.OK)
+    public DeviceDTO retrieveById(@PathVariable String id){
+        return deviceService.retrieveById(id);
     }
 
-    @GetMapping(value = "/device/v1/list")
-    public List<DeviceDTO> list(){
-        return deviceService.list();
+    @GetMapping(value = "/device/v1/retrieveAll")
+    @ResponseStatus(HttpStatus.OK)
+    public List<DeviceDTO> retrieveAll(){
+        return deviceService.retrieveAll();
+    }
+
+    @GetMapping(value = "/device/v1/retrieveByBrand/{brand}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<DeviceDTO> retrieveByBrand(@PathVariable String brand){
+        return deviceService.retrieveByBrand(brand);
+    }
+
+    @GetMapping(value = "/device/v1/retrieveByState/{state}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<DeviceDTO> retrieveByState(@PathVariable StateEnum state){
+        return deviceService.retrieveByState(state);
     }
 
     @PutMapping(value = "/device/v1/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public DeviceDTO update(@PathVariable String id, @RequestBody DeviceDTO deviceDTO){
-        return deviceService.update(deviceDTO);
+        return deviceService.update(id, deviceDTO);
     }
 
     @DeleteMapping(value = "/device/v1/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String id){
         deviceService.delete(id);
     }
